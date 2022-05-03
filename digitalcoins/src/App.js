b5298075e3dd6f5e75940d84910d2101b21ec43b
Hamid Azimi <Hamidazimi.azp@gmail.com>
2022-05-03 12:28:28 +0430

    add redux store to app

--- digitalcoins/src/App.js
+++ digitalcoins/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import configureStore from './redux/store';
+import { Provider } from "react-redux";
 
-function App() {
+const store = configureStore();
+
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <>
+        Hello World !
+      </>
+    </Provider>
   );
 }
 
