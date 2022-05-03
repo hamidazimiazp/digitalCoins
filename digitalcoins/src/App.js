de6194c6e1292ebc93c8cc182465e0c3adb33146
Hamid Azimi <Hamidazimi.azp@gmail.com>
2022-05-03 14:40:15 +0430

    move redux provider from app to index

--- digitalcoins/src/App.js
+++ digitalcoins/src/App.js
@@ -1,18 +1,13 @@
 import './App.css';
-import configureStore from './redux/store';
-import { Provider } from "react-redux";
 import Main from './container/main';
 
-const store = configureStore();
 
 const App = () => {
 
   return (
-    <Provider store={store}>
-      <>
-        <Main />
-      </>
-    </Provider>
+    <>
+      <Main />
+    </>
   );
 }
 
--- digitalcoins/src/index.js
+++ digitalcoins/src/index.js
@@ -4,13 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import configureStore from './redux/store';
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+const store = configureStore();
+
+
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider >
 );
 
 // If you want to start measuring performance in your app, pass a function
