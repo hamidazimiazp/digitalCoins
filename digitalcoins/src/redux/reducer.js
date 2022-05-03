4b7f8963cd8a9c731710d1ff77782d7497f082b3
Hamid Azimi <Hamidazimi.azp@gmail.com>
2022-05-03 14:50:23 +0430

    use break in case and fix bug

--- digitalcoins/src/redux/reducer.js
+++ digitalcoins/src/redux/reducer.js
@@ -4,7 +4,8 @@ import * as actionTypes from "./action_types";
 const reducer = (state = [], action) => {
     switch (action.type) {
         case actionTypes.recievedCoins:
-            state = action.payload.data
+            state = action.payload.data;
+            break;
         default:
             return state
     }
