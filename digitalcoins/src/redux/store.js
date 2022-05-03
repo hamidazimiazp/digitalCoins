babb3acf953e37424d552eb7018d3e9f8f4fb0c6
Hamid Azimi <Hamidazimi.azp@gmail.com>
2022-05-03 12:20:44 +0430

    create store

---
+++ digitalcoins/src/redux/store.js
@@ -0,0 +1,13 @@
+import { applyMiddleware, createStore } from "redux";
+import thunk from 'redux-thunk'
+import reducer from "./reducer";
+import api from "./middlewares/api";
+
+export default function configureStore() {
+    const store = createStore(
+        reducer,
+        applyMiddleware([thunk, api])
+    );
+
+    return store;
+}
