8839e2c720d9c3bd1203e1238e78e1026ddcb905
Hamid Azimi <Hamidazimi.azp@gmail.com>
2022-05-03 14:41:37 +0430

    complete actions

--- digitalcoins/src/redux/actions.js
+++ digitalcoins/src/redux/actions.js
@@ -1,11 +1,38 @@
 import * as actionTypes from "./action_types";
 
 
+export const coinsRequested = () => {
+    return {
+        type: actionTypes.coinsRequested,
+    }
+}
+
+
+export const APICallBegan = (data) => {
+    return {
+        type: actionTypes.apiCallBegan,
+        payload: data
+    }
+}
+
+
+export const APICallSuccess = () => {
+    return {
+        type: actionTypes.apiCallSuccess,
+    }
+}
+
+
+export const APICallFaild = () => {
+    return {
+        type: actionTypes.apiCallFaild,
+    }
+}
+
+
 export const recievedCoins = (data) => {
     return {
         type: actionTypes.recievedCoins,
-        payload: {
-            data
-        }
+        payload: data
     }
 }
