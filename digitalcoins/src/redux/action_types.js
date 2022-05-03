d2bde7137a18838e350fa96022dc28b7b9d6c1f0
Hamid Azimi <Hamidazimi.azp@gmail.com>
2022-05-03 14:41:01 +0430

    add new action types

--- digitalcoins/src/redux/action_types.js
+++ digitalcoins/src/redux/action_types.js
@@ -1,4 +1,5 @@
 export const apiCallBegan = "api/callBegan";
 export const apiCallSuccess = "api/callSuccess";
 export const apiCallFaild = "api/callFaild";
+export const coinsRequested = "coins/coinsRequested";
 export const recievedCoins = "coins/recievedCoins";
