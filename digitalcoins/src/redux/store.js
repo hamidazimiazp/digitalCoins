d36564d3741a2d16e7fa9c7049a8401b867969ec
Hamid Azimi <Hamidazimi.azp@gmail.com>
2022-05-03 14:43:50 +0430

    add redux devtools to store

--- digitalcoins/src/redux/store.js
+++ digitalcoins/src/redux/store.js
@@ -2,11 +2,15 @@ import { applyMiddleware, createStore } from "redux";
 import thunk from 'redux-thunk'
 import reducer from "./reducer";
 import api from "./middlewares/api";
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 
 export default function configureStore() {
     const store = createStore(
         reducer,
-        applyMiddleware(thunk, api)
+        composeWithDevTools(
+            applyMiddleware(thunk, api),
+        ),
     );
 
     return store;
