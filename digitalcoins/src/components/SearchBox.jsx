0db50407c406af1c30f94a5367ab4d3d872ac6c2
Hamid Azimi <Hamidazimi.azp@gmail.com>
2022-05-14 17:19:20 +0430

    add base styles to index

--- digitalcoins/src/components/SearchBox.jsx
+++ digitalcoins/src/components/SearchBox.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { TextField } from '@mui/material';
+import "./SearchBox.sass";
 
 
 const SearchBox = ({ onChangeHandler }) => {
--- digitalcoins/src/index.js
+++ digitalcoins/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import configureStore from './redux/store';
 import { Provider } from "react-redux";
+import "./base.scss";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
