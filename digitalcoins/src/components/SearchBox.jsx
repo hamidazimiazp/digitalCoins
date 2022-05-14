7c1a21892ba22da75dcc90b2e9e3bbd54647b40f
Hamid Azimi <Hamidazimi.azp@gmail.com>
2022-05-14 16:57:14 +0430

    change input to textField in Search Box Comp

--- digitalcoins/src/components/SearchBox.jsx
+++ digitalcoins/src/components/SearchBox.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { TextField } from '@mui/material';
 
 
 const SearchBox = ({ onChangeHandler }) => {
 
     return (
         <>
-            <input type="search" onChange={(e) => onChangeHandler(e)} />
+            <TextField
+                id=""
+                label="Search Coins"
+                variant="outlined"
+                color='info'
+                onChange={(e) => onChangeHandler(e)}
+            />
         </>
     );
 }
