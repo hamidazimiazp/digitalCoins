e7c62158ec6272035223627297a0b9273d5d72b4
Hamid Azimi <Hamidazimi.azp@gmail.com>
2022-05-14 17:38:59 +0430

    update api link

--- digitalcoins/src/redux/api.js
+++ digitalcoins/src/redux/api.js
@@ -3,7 +3,7 @@ import { APICallBegan } from "./actions";
 
 
 
-const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=200&page=1&sparkline=false";
+const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=300&page=1&sparkline=false";
 
 
 export const loadCoins = () => (dispatch, getState) => {
